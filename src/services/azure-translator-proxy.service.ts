547c7da20ed0f2a5051e67895ac0a61e8f9e3163
Edi Wang <Edi.Wang@outlook.com>
2024-06-13 11:54:37 +0800

    use api proxy

--- src/services/azure-translator-proxy.service.ts
+++ src/services/azure-translator-proxy.service.ts
@@ -8,10 +8,8 @@ import { HttpClient } from '@angular/common/http';
 export class AzureTranslatorProxyService {
     constructor(private http: HttpClient) { }
 
-    apiEndpoint: string = 'https://edi-translator-api.azurewebsites.net';
-
     translate(request: TranslationRequest) {
-        let url = `${this.apiEndpoint}/api/translation/translate`;
+        let url = `/api/translation/translate`;
         return this.http.post(url, request);
     }
 }
